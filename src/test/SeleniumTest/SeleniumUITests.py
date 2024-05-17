64a0cd4cd0d5b8f611b699b6aa6dd85eef3b8afd
moffd234 <moffd234@gmail.com>
2024-05-17 11:31:26 -0400

    Added guest test but it currently can't find login button

--- src/test/SeleniumTest/SeleniumUITests.py
+++ src/test/SeleniumTest/SeleniumUITests.py
@@ -1,15 +1,14 @@
 import unittest
-
 import os
-import time
-import selenium.common
+import selenium
 from selenium import webdriver
-from dotenv import load_dotenv, find_dotenv
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager  # pip install webdriver-manager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementNotInteractableException, JavascriptException
+from dotenv import load_dotenv, find_dotenv
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
-
-
 class MyTestCase(unittest.TestCase):
 
     @classmethod
@@ -21,6 +20,7 @@ class MyTestCase(unittest.TestCase):
         chrome_options.add_experimental_option(name="detach", value=True)
         service = ChromeService(executable_path=chrome_driver_path)
         cls.driver = webdriver.Chrome(service=service, options=chrome_options)
+        cls.driver.maximize_window()
 
     def setUp(self):
         # This method runs before each test method
@@ -30,14 +30,13 @@ class MyTestCase(unittest.TestCase):
         error_count: int = 0
         try:
             login_button = self.driver.find_element(by=By.XPATH,
-                                                    value='//*[@id="root"]/div/main/section/div/div/div[2]/div/div['
-                                                          '2]/div/div[1]/div[1]/div/div[2]/h3/a')
+                                                    value='//*[@id="root"]/div/form/button[1]')
             login_button.click()
 
-            username = self.driver.find_element(by=By.XPATH, value='//*[@id="root"]/div/div/form/div[1]/input')
+            username = self.driver.find_element(by=By.XPATH, value='//*[@id="root"]/div/div/form/div[2]/input')
             username.send_keys(os.getenv("ECHO_USERNAME"))  # Gets the echo_username from .env
 
-            password = self.driver.find_element(by=By.XPATH, value='//*[@id="root"]/div/div/form/div[2]/input')
+            password = self.driver.find_element(by=By.XPATH, value='//*[@id="root"]/div/div/form/div[3]/input')
             password.send_keys(os.getenv("ECHO_PASSWORD"))  # Gets the echo_password from .env
 
             login_button = self.driver.find_element(by=By.CLASS_NAME, value='btn-primary')
@@ -46,7 +45,33 @@ class MyTestCase(unittest.TestCase):
         except selenium.common.JavascriptException as e:
             error_count += 1
 
+        expected: str = "http://localhost:8314/homepage"
+        url: str = self.driver.current_url
+
         self.assertEqual(error_count, 0)
+        self.assertEqual(expected, url)
+
+    def test_guest_login(self):
+        error_count: int = 0
+        try:
+            login_button = self.driver.find_element(by=By.XPATH,
+                                                    value='//*[@id="root"]/div/form/button[1]')
+            login_button.click()
+            # Wait for the guest button to be visible and clickable
+            guest_button = WebDriverWait(self.driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div/form/p[2]/a'))
+            )
+            guest_button.click()
+
+        except (selenium.common.JavascriptException, selenium.common.TimeoutException)as e:
+            error_count += 1
+            print(f"Exception: {e}")
+
+        expected: str = "http://localhost:8314/homepage"
+        url: str = self.driver.current_url
+
+        self.assertEqual(error_count, 0)
+        self.assertEqual(expected, url)
 
     @classmethod
     def tearDownClass(cls):
